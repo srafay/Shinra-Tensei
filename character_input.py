c168d1013ff300c012436f3fbdc7a7c09bb13807
Syed Rafay <rafay_syed1@yahoo.com>
2018-06-04 22:24:05 +0500

    Add on to the previous program by asking the user for another number and printing out that many copies of the previous message

--- character_input.py
+++ character_input.py
@@ -8,4 +8,10 @@ age = input("Enter your age: ")
 now = datetime.datetime.now()
 #now.year, now.month, now.day, now.hour, now.minute, now.second
 
-print("\nYou will be 100 years old in the year: " + str((100 - int(age) + now.year)))
+# EXTRA
+# Add on to the previous program by asking the user for another number
+# and printing out that many copies of the previous message
+
+number = input("\nEnter a number: ")
+
+print(int(number) * ("\nYou will be 100 years old in the year: " + str((100 - int(age) + now.year))))
