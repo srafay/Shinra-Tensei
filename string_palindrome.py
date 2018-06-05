6183f7b832669530171c883d0a6064dde54129ad
Syed Rafay <rafay_syed1@yahoo.com>
2018-06-05 21:58:52 +0500

    Measuring performance of different methods to check if a string is palindrome

--- string_palindrome.py
+++ string_palindrome.py
@@ -1,5 +1,7 @@
 # Ask the user for a string and print out whether this string is a palindrome or not
 
+import timeit
+
 def palindrome(string):
     for i in range(0, len(string)):
         if (string[i] == string[len(string)-i-1]):
@@ -9,7 +11,28 @@ def palindrome(string):
     return 1
             
 string = input("Enter a string: ")
+
+start = timeit.timeit()
+
 if (palindrome(string)):
     print("{} is palindrome!" .format(string))
 else:
     print("{} is not a palindrome!" .format(string))
+
+end = timeit.timeit()
+print ("palindrome() took: {} " .format(end - start))
+
+
+# Efficient
+
+def fasterPalindrome(string):
+    return (string == string[::-1])
+
+start = timeit.timeit()
+if (fasterPalindrome(string)):
+    print("{} is palindrome!" .format(string))
+else:
+    print("{} is not a palindrome!" .format(string))
+    
+end = timeit.timeit()
+print ("fasterPalindrome() took: {} " .format(end - start))
