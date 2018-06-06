7b84653b2bfd22820d0277b6c9029ab98115dd4c
Syed Rafay <rafay_syed1@yahoo.com>
2018-06-06 20:59:22 +0500

    Taking sqrt of the number for efficient prime checking, can be improved by following AKS algorithm

--- check_prime.py
+++ check_prime.py
@@ -1,19 +1,54 @@
 # Ask the user for a number and determine whether the number is prime or not
 
+import timeit
+import math
+
 def isPrime(number):
     if (number % 2 == 0):
-        return 0
+        return False
     else:
         i = 3
         while i < number:
             if (number % i == 0):
-                return 0
+                return False
             else:
                 i += 2
-        return 1
+        return True
 
 number = int(input("Enter a number: "))
-if isPrime(number):
+
+start = timeit.timeit()
+
+result = isPrime(number)
+
+end = timeit.timeit()
+print ("isPrime() took: {} " .format(start - end))
+
+if result:
     print("{} is a prime number!" .format(number))
 else:
     print("{} is not a prime number!" .format(number))
+
+
+def sqrtPrime(number):
+    if number % 2 == 0:
+        return False
+    if number % 3 == 0:
+        return False
+    i = 5
+    sqrt = math.sqrt(number)
+    while i < sqrt:
+        if number % i == 0:
+            return False
+        i += 2
+    return True
+
+_start = timeit.timeit()
+
+result = isPrime(number)
+
+_end = timeit.timeit()
+print ("sqrtPrime() took: {} " .format(_start - _end))
+
+
+print ("Speed up is {} " .format((start - end) / (_start - _end)))
