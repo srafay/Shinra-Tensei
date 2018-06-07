c14ca7369f1ab4efa10c6d21588844c0e4a2d2c2
Syed Rafay <rafay_syed1@yahoo.com>
2018-06-07 05:15:09 +0500

    Improvement to the fibonacci program by exploiting list properties

--- fibonacci.py
+++ fibonacci.py
@@ -5,16 +5,12 @@ def fibonacci(count):
         return [1]
     if (count == 2):
         return [1, 1]
-    _fib = [1, 1]
-    x = y = _sum = 1
-    _count = 2
-    while _count < count:
-        _sum = x + y
-        _fib.append(_sum)
-        x = y
-        y = _sum
+    fib = [1, 1]
+    _count = 0
+    while _count < count-2:
+        fib.append(fib[_count] + fib[_count + 1])
         _count += 1
-    return _fib
+    return fib
 
 count = int(input("Count of Fibonacci series: "))
 print (fibonacci(count))
