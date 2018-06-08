3100a0ea7bfebab623ed511fad13e380cab4e1ef
Syed Rafay <rafay_syed1@yahoo.com>
2018-06-09 04:32:23 +0500

    A cryptographically secure random password generator

--- password_generator.py
+++ password_generator.py
@@ -9,3 +9,14 @@ def getPass(length):
 length = int(input("Enter the length of password: "))
 password = getPass(length)
 print (password)
+
+
+#import secrets
+
+# A more secure method for generating a cryptographically secure passwords
+def getSafePass(length):
+    
+    #password = ''.join(secrets.choice(string.ascii_letters + string.digits) for x in range(10))
+    
+    # Since secrets is not supported in py 3.5, just use SystemRandom()
+    password = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for x in range(10))
