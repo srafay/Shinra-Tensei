28615d56cc2cf8953d665ceb5ee2adb203e1d151
Syed Rafay <rafay_syed1@yahoo.com>
2021-12-01 03:08:28 +0930

    Blazor - Added a Todo Page and learned data binding

--- aspdotnet/blazor/Blazor Counter and TODO/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ aspdotnet/blazor/Blazor Counter and TODO/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,70 +13,70 @@ namespace Blazor_Counter_and_TODO.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 1 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 2 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 3 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 4 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 5 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 6 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 7 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 8 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 9 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Blazor_Counter_and_TODO;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 10 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Blazor_Counter_and_TODO.Shared;
 
 #line default
@@ -91,7 +91,7 @@ using Blazor_Counter_and_TODO.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\Pages\Counter.razor"
+#line 9 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\Pages\Counter.razor"
        
     private int currentCount = 0;
 
--- aspdotnet/blazor/Blazor Counter and TODO/obj/Debug/net5.0/RazorDeclaration/Pages/Todo.razor.g.cs
+++ aspdotnet/blazor/Blazor Counter and TODO/obj/Debug/net5.0/RazorDeclaration/Pages/Todo.razor.g.cs
@@ -13,75 +13,76 @@ namespace Blazor_Counter_and_TODO.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 1 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 2 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 3 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 4 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 5 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 6 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 7 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 8 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 9 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Blazor_Counter_and_TODO;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "d:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
+#line 10 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\_Imports.razor"
 using Blazor_Counter_and_TODO.Shared;
 
 #line default
 #line hidden
 #nullable disable
+    [Microsoft.AspNetCore.Components.RouteAttribute("/todo")]
     public partial class Todo : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
@@ -89,6 +90,26 @@ using Blazor_Counter_and_TODO.Shared;
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 19 "D:\Projects\Shinra-Tensei\aspdotnet\blazor\Blazor Counter and TODO\Pages\Todo.razor"
+       
+
+    private List<TodoItem> todoItems = new();
+    public string newTodo;
+
+    public void AddTodo()
+    {
+        if (!String.IsNullOrWhiteSpace(newTodo))
+        {
+            todoItems.Add(new TodoItem {Title = newTodo});
+            newTodo = String.Empty;
+        }
+    }
+
+
+#line default
+#line hidden
+#nullable disable
     }
 }
 #pragma warning restore 1591
